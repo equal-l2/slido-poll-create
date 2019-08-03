be0bfe0863700d94b9cb993d9891622687574d30
equal-l2 <eng.equall2@gmail.com>
2019-08-04 03:16:33 +0900

    change

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-function hello() {
-    return "hello world";
+import pup = require('puppeteer');
+
+async function run() {
+    const browser = await pup.launch();
+    const page = await browser.newPage();
+    await page.goto("http://github.com/");
 }
 
-
-console.log(hello());
+run().then(()=>{});
